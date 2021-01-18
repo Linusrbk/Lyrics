02b9ad69786a7b1b92fbed3b919cef5d261acf3d
Linusrbk <linus.astrom@elev.ga.ntig.se>
2021-01-18 13:39:21 +0100

    Create lyrics.py

---
+++ lyrics.py
@@ -0,0 +1,51 @@
+lyric="""In the darkest silence
+Sothing seems to satisfy
+Shout of noise, Brain the mad
+So fly freedom eternity
+
+Screaming out of the world
+Destroy misery
+Trying high, white out
+Judgement day is the forgot
+
+The endless time in mind
+Even if life comes to an end
+The ring sound is eternal for my voice
+Open the arms
+
+My heart and soul's live in the waves drive
+Fly high torn feeling moment for the all myself
+Flash back the long of distance, many battle ray
+Try to after fire Louder than steel
+
+Feeling ray to avenge
+Return to my beat sound
+Shout of noise, brain the mad
+So fly freedom eternity
+
+Wave is come to mind
+Even if life comes to an end
+The rising sun is eternal for my blood
+Open the arms
+
+My heart and soul's live in the waves drive
+Fly high torn feeling moment for the all myself
+Flash back the long of distance, many battle ray
+Try to after fire Louder than steel
+
+The endless time in mind
+Even if life comes to an end
+The ring sound is eternal for my voice
+Open the arms
+
+My heart and soul's live in the waves drive
+Fly high torn feeling moment for the all myself
+Flash back the long of distance, many battle ray
+Try to after fire Louder than steel"""
+
+
+in1=input('vad ska bli annat?')
+in2=input('vad ska det bli?')
+lyric2=lyric[0:1000].lower().replace(in1,in2)
+#lyric2=lyric[0:100000].lower().splitlines()#.replace(in1,in2)
+print(lyric2)
